013c5dc21292c54cd87eff0d968e1f61f2396154
Abhishek Kumar <abhiroshan692@gmail.com>
2023-01-09 14:00:20 +0530

    added new content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,14 @@ import { Fragment } from "react";
 
 function About() {
   return (
+    <div className="font-bold text-2xl text-center">
       <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
       <p>Version: 1.0.0</p>
+      <a className="underline decoration-blue-500 text-white" href={"www.google.com"}>DEVELOPED BY ABHISHEK</a>
     </Fragment>
+    </div>
   )
 }
 
